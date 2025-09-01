cf842750a902187433fcc1f2d35ae7384c4f52fc
MAKARYAN Raphael <53433445+raphaelmakaryan@users.noreply.github.com>
2025-09-01 15:00:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
-const core = require('@actions/core');
 const axios = require('axios');
-const debug = true;
+const core = require('@actions/core');
 
-try {
-    const username = core.getInput('username');
-    const apiToken = core.getInput('api-token');
-    if(debug){
-        console.log("User : " + username)
-        console.log("TOKEN : " + apiToken)
-    }
+async function run() {
+    try {
+        const username = core.getInput('username');
+        const apiToken = core.getInput('api-token');
+        console.log("User:", username);
+        console.log("TOKEN:", apiToken);
 
-    const url = `https:///www.pythonanywhere.com/api/v0/user/${username}/webapps/${username}.pythonanywhere.com/reload/`
-    if(debug){
-        console.log("URL : " + url)
-    }
+        const url = `https://www.pythonanywhere.com/api/v0/user/${username}/webapps/${username}.pythonanywhere.com/reload/`;
+        console.log("URL:", url);
 
-    console.log('Trying to reload webapp...')
-    
-    axios.post(url, null, {
-        headers: {"Authorization" : `Token ${apiToken}`}
-    })
-    .then(function (response) {
-        console.log('Reloaded webapp successfully.')
-    })
-    .catch(function (error) {
-        console.log("Une erreur est survenue : ", error)
+        console.log('Trying to reload webapp...');
+        const response = await axios.post(url, null, {
+            headers: {"Authorization" : `Token ${apiToken}`}
+        });
+
+        console.log("✅ Reloaded webapp successfully:", response.status);
+
+    } catch (error) {
+        console.error("❌ Une erreur est survenue :", error.response ? error.response.data : error.message);
         core.setFailed(error.message);
-    });
-} catch (error) {
-    console.log("Une erreur est survenue : ", error)
-    core.setFailed(error.message);
+    }
 }
+
+run();
