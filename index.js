63235159880a5754e4ce1c093075d2945d132ec4
MAKARYAN Raphael <53433445+raphaelmakaryan@users.noreply.github.com>
2025-09-01 14:56:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const core = require('@actions/core');
 const axios = require('axios');
+const debug = true;
 
 try {
     const username = core.getInput('username');
     const apiToken = core.getInput('api-token');
+    if(debug){
+        console.log("User : " + username)
+        console.log("TOKEN : " + apiToken)
+    }
 
     const url = `https:///www.pythonanywhere.com/api/v0/user/${username}/webapps/${username}.pythonanywhere.com/reload/`
+    if(debug){
+        console.log("URL : " + url)
+    }
 
     console.log('Trying to reload webapp...')
     
@@ -16,9 +24,10 @@ try {
         console.log('Reloaded webapp successfully.')
     })
     .catch(function (error) {
-        core.setFailed(error.message);
         console.log("Une erreur est survenue : ", error)
+        core.setFailed(error.message);
     });
 } catch (error) {
+    console.log("Une erreur est survenue : ", error)
     core.setFailed(error.message);
 }
