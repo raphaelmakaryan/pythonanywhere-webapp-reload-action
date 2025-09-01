1b5a9c5e7fe7358ebd687b394be4fd9cad8c6ae8
MAKARYAN Raphael <53433445+raphaelmakaryan@users.noreply.github.com>
2025-09-01 14:47:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const core = require('@actions/core');
 const axios = require('axios');
 
 try {
-    const host = core.getInput('host');
     const username = core.getInput('username');
     const apiToken = core.getInput('api-token');
-    const domainName = core.getInput('domain-name');
 
-    const url = `https://${host}/api/v0/user/${username}/webapps/${domainName}/reload/`
+    const url = `https:///www.pythonanywhere.com/api/v0/user/${username}/webapps/${username}.pythonanywhere.com/reload/`
 
     console.log('Trying to reload webapp...')
     
