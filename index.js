89aa9050a24ce93218ac6ed37dda6bae07648acc
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-09-01 15:14:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const axios = require('axios');
-const core = require('@actions/core');
+try {
+    const username = core.getInput('username');
+    const apiToken = core.getInput('api-token');
+    console.log("User:", username);
+    console.log("TOKEN:", apiToken);
 
-async function run() {
-    try {
-        const username = core.getInput('username');
-        const apiToken = core.getInput('api-token');
-        console.log("User:", username);
-        console.log("TOKEN:", apiToken);
+    const url = `https://www.pythonanywhere.com/api/v0/user/${username}/webapps/${username}.pythonanywhere.com/reload/`;
+    console.log("URL:", url);
 
-        const url = `https://www.pythonanywhere.com/api/v0/user/${username}/webapps/${username}.pythonanywhere.com/reload/`;
-        console.log("URL:", url);
+    console.log('Trying to reload webapp...');
 
-        console.log('Trying to reload webapp...');
-        const response = await axios.post(url, null, {
-            headers: {"Authorization" : `Token ${apiToken}`}
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Token ${apiToken}`
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("✅ Reloaded webapp successfully:", data);
+        })
+        .catch(error => {
+            console.error("❌ Une erreur est survenue :", error);
         });
 
-        console.log("✅ Reloaded webapp successfully:", response.status);
-        console.log("Response data:", response.data);
-
-    } catch (error) {
-        console.error("❌ Une erreur est survenue :", error);
-        if (error.response) {
-            console.error("Status:", error.response.status);
-            console.error("Data:", error.response.data);
-        }
-        core.setFailed(error.message);
+} catch (error) {
+    console.error("❌ Une erreur est survenue :", error);
+    if (error.response) {
+        console.error("Status:", error.response.status);
+        console.error("Data:", error.response.data);
     }
 }
-
-run();
